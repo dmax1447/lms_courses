f3d7f17c2860815979833916c743b661028f8e5b
Max Dragaytsev <40897499+dmax1447@users.noreply.github.com>
2024-02-26 12:33:08 +0300

    add some styles

--- src/App.vue
+++ src/App.vue
@@ -50,12 +50,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
 .header {
   display: flex;
   gap: 16px;
